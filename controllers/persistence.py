90078f92b6c47e7c3fc444baac0b2f47bd1f2257
Luiz Joaquim Aderaldo Amichi <luizamichi@gmail.com>
2020-09-10 18:20:25 -0300

    Versão 1.2

---
+++ controllers/persistence.py
@@ -0,0 +1,60 @@
+import sqlite3
+from config import DATABASE_URI
+
+# Inserir tupla na tabela
+def create(query):
+	with sqlite3.connect(DATABASE_URI) as connection:
+		cursor = connection.cursor()
+		try:
+			cursor.execute(query)
+			connection.commit()
+			return cursor.rowcount
+		except:
+			return False
+
+# Procurar tuplas na tabela
+def read(query):
+	with sqlite3.connect(DATABASE_URI) as connection:
+		cursor = connection.cursor()
+		cursor.execute(query)
+		return cursor.fetchall()
+
+# Procurar tupla na tabela
+def read_one(query):
+	with sqlite3.connect(DATABASE_URI) as connection:
+		cursor = connection.cursor()
+		cursor.execute(query)
+		return cursor.fetchone()
+
+# Alterar tupla da tabela
+def update(query):
+	with sqlite3.connect(DATABASE_URI) as connection:
+		cursor = connection.cursor()
+		try:
+			cursor.execute(query)
+			connection.commit()
+			return cursor.rowcount
+		except:
+			return False
+
+# Remover tupla da tabela
+def delete(query):
+	with sqlite3.connect(DATABASE_URI) as connection:
+		cursor = connection.cursor()
+		cursor.execute(query)
+		return cursor.rowcount
+
+# Exportar tabelas a partir de um arquivo DB
+def export(filename):
+	with open(filename, 'w') as file:
+		connection = sqlite3.connect(DATABASE_URI)
+		for row in connection.iterdump():
+			file.write('%s\n' % row)
+
+# Importar tabelas a partir de um arquivo SQL
+def matter(filename):
+	with open(filename, 'r') as file:
+		connection = sqlite3.connect(DATABASE_URI)
+		cursor = connection.cursor()
+		cursor.executescript(file.read())
+		connection.commit()
